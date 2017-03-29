9c9760d8ff33c5e81bbcf525488672ca87c4c761
DiamondDat <diamond.dat@1pac.vn>
2017-03-29 14:10:28 +0700

    Add confirmation to users :email:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name,  presence: true, length: { minimum: 5, maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    uniqueness: { case_sensitvie: false }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
 end
