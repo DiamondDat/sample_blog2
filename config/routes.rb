ce2e2a2282891d547c3b7b6b5e4ad4674be80f1d
DiamondDat <diamond.dat@1pac.vn>
2017-03-30 09:04:43 +0700

    Add views to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+  root to: 'static_pages#home'
+
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
   devise_for :users
-  resources :users, only: [:show, :index] do
-    post :generate_new_password_email
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
