9a2f3f54e792774a7a510a4cb81e9affbc242738
DiamondDat <diamond.dat@1pac.vn>
2017-04-05 13:32:59 +0700

    Update like and relationships javascript

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,12 @@ Rails.application.routes.draw do
     end
   end
   resources  :microposts,    only: [:create, :destroy] do
-    resources  :likes,       only: [:create, :destroy]
     resources  :comments,    only: [:create, :destroy]
   end
   resources  :comments do
     resources  :comments,    only: [:create, :destroy]
   end
+  resources  :likes,         only: [:create, :destroy]
   resources  :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
